3dbfc5f50a63e554f62a7d5d47ea09dbbd66c0d1
Ryan Maloney <rypmaloney@gmail.com>
2022-02-12 11:30:49 -0500

    Displays messages using EJS.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  },
+  {
+    text: "Oh boy!",
+    user: "Rick",
+    added: new Date()
+  },
+  {
+    text: "What. it. do!",
+    user: "Stacy",
+    added: new Date()
+  },
+];
+
+
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Message Board', messages: messages });
 });
 
 module.exports = router;
